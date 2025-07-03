51a8da7297a5aed250c5dd561cccfbba59f5df87
marcginestet <m.ginestet@qq.com>
2025-07-03 22:54:02 +0800

    Quartz sync: Jul 3, 2025, 10:54 PM

--- quartz/plugins/transformers/links.ts
+++ quartz/plugins/transformers/links.ts
@@ -25,7 +25,7 @@ interface Options {
 }
 
 const defaultOptions: Options = {
-  markdownLinkResolution: "shortest",
+  markdownLinkResolution: "relative",
   prettyLinks: true,
   openLinksInNewTab: false,
   lazyLoad: false,
